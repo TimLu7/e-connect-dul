5b9ef623bfa1c9c118e680d70deb45b2be6ab33e
TimLu7 <90731993+TimLu7@users.noreply.github.com>
2022-12-14 23:47:03 -0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
+//The frontend implementation with React.js is also a good choice, 
+//as it allows for efficient rendering of components and updates to the user interface.
+//The use of routes to communicate with the backend Express server is a standard approach, 
+//and should work well for this application.
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
